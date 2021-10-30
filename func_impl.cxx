a3d03a3e7a55b189dbacb49cac37a5056f968e13
Kostiantyn Ponomarenko <kp@koponomarenko.com>
2021-10-30 12:25:26 +0300

    init - working in Docker / remote

---
+++ func_impl.cxx
@@ -0,0 +1,9 @@
+module;
+
+import <iostream>;
+
+module mod;
+
+void func() {
+    std::cout << "hello, world, from C++ module!\n";
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+/*
+ * Based on https://blog.feabhas.com/2021/08/c20-modules-with-gcc11/
+ */
+import mod;
+
+int main(){
+    func();
+}
